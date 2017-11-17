1f927ed45313da7f1dfbacce47db977e0980268b
Tofiz <tofi.zagon@gmail.com>
2017-11-17 09:58:28 +0100

    feat(pallida-orientation-exam): backend server set up, mysql connection is created

---
+++ backend.js
@@ -0,0 +1,20 @@
+'use strict' 
+
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/', express.static("./public"));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'examdatabase'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
